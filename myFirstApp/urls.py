536873a486cd4f0303f6c1458314ce1807e8295f
veronicavonk <32149512+veronicavonk@users.noreply.github.com>
2020-06-23 01:38:01 -0500

    Detalle del post

--- myFirstApp/urls.py
+++ myFirstApp/urls.py
@@ -4,5 +4,6 @@ from . import views  # importand todas las vistas
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), #asociando una vista llamada post_list
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
 
