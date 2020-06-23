17ade2da6253c329f53343d92c2b2435476c2d0e
veronicavonk <32149512+veronicavonk@users.noreply.github.com>
2020-06-23 17:37:01 -0500

    Permisos para

--- myFirstApp/urls.py
+++ myFirstApp/urls.py
@@ -5,5 +5,7 @@ from . import views  # importand todas las vistas
 urlpatterns = [
     path('', views.post_list, name='post_list'), #asociando una vista llamada post_list
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 
